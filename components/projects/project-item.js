40a505aedeb6874d361f4e8fe9828547cd509679
PMtHk <zooby88@naver.com>
2022-07-22 21:28:24 +0900

    태그 사이즈 수정

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -4,7 +4,7 @@ export default function ProjectItem({ data }) {
   const title = data.properties.Name.title[0]?.plain_text;
   const github = data.properties.Github.url;
   const description = data.properties.Description.rich_text[0]?.plain_text;
-  const imgSrc = data.cover.file?.url || data.cover.external.url ;
+  const imgSrc = data.cover.file?.url || data.cover.external.url;
   const tags = data.properties.Tags?.multi_select;
   const start = data.properties.WorkPeriod?.date?.start;
   const end = data.properties.WorkPeriod.date?.end;
@@ -29,10 +29,10 @@ export default function ProjectItem({ data }) {
         <p className="my-1 ">
           작업기간 : {start} ~ {end}
         </p>
-        <div className="flex items-start mt-2">
+        <div className="flex items-start flex-row mt-2">
           {tags.map((aTag) => (
             <h1
-              className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30"
+              className="px-2 py-1 mr-1 rounded-lg bg-sky-200 dark:bg-sky-700 w-30"
               key={aTag.id}
             >
               {aTag.name}
