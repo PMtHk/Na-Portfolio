4dc55def7091faf4e6b10857599008ad516baa90
PMtHk <zooby88@naver.com>
2022-08-02 20:43:12 +0900

    노션 커버이미지 관련 버그 수정

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -1,38 +1,38 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function ProjectItem({ data }) {
   const title = data.properties.Name.title[0]?.plain_text;
   const github = data.properties.Github.url;
   const description = data.properties.Description.rich_text[0]?.plain_text;
-  const imgSrc = data.cover.file?.url || data.cover.external.url;
+  const imgSrc = data?.cover?.file.url || data?.cover?.external.url;
   const tags = data.properties.Tags?.multi_select;
   const start = data.properties.WorkPeriod?.date?.start;
-  const end = data.properties.WorkPeriod.date?.end;
+  const end = data.properties.WorkPeriod?.date?.end;
 
   return (
-    <div className="project-card max-w-lg min-w-full">
+    <div className='project-card max-w-lg min-w-full'>
       <Image
-        className="rounded-t-xl"
+        className='rounded-t-xl'
         src={imgSrc}
-        alt="cover image"
-        width="100%"
-        height="50%"
-        layout="responsive"
-        objectFit="cover"
+        alt='cover image'
+        width='100%'
+        height='50%'
+        layout='responsive'
+        objectFit='cover'
         quality={100}
       />
 
-      <div className="p-4 flex flex-col">
-        <h1 className="text-2xl font-bold">{title}</h1>
-        <h3 className="mt-4 text-xl">{description}</h3>
+      <div className='p-4 flex flex-col'>
+        <h1 className='text-2xl font-bold'>{title}</h1>
+        <h3 className='mt-4 text-xl'>{description}</h3>
         <a href={github}>Github Link</a>
-        <p className="my-1 ">
+        <p className='my-1 '>
           작업기간 : {start} ~ {end}
         </p>
-        <div className="flex items-start flex-wrap mt-2">
+        <div className='flex items-start flex-wrap mt-2'>
           {tags.map((aTag) => (
             <h1
-              className="px-2 py-1 mr-1 my-1 rounded-lg bg-sky-200 dark:bg-sky-700 w-30"
+              className='px-2 py-1 mr-1 my-1 rounded-lg bg-sky-200 dark:bg-sky-700 w-30'
               key={aTag.id}
             >
               {aTag.name}
