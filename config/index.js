8ce4b350ffe2b144f45d13aa9ebbbf08a2997563
PMtHk <zooby88@naver.com>
2022-09-22 18:19:49 +0900

    config 에 스터디DB 추가

--- config/index.js
+++ config/index.js
@@ -1,2 +1,3 @@
-export const DATABASE_ID = process.env.NOTION_DATABASE_ID;
+export const PDATABASE_ID = process.env.NOTION_PDATABASE_ID;
+export const SDATABASE_ID = process.env.NOTION_SDATABASE_ID;
 export const TOKEN = process.env.NOTION_TOKEN;
